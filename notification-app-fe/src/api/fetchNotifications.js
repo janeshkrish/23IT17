43172529358be0e94ff94cd0bc69a6baa843cad5
janeshkrish <janeshkrishna12@gmail.com>
2026-06-30 12:59:40 +0530

    fix: parameter for page and filter given

--- notification-app-fe/src/api/fetchNotifications.js
+++ notification-app-fe/src/api/fetchNotifications.js
@@ -1,15 +1,16 @@
 import React from "react";
-import axios from 'axios';
-export function fetchNotifications() {
+import axios from "axios";
+export function fetchNotifications(page,filter) {
     try{
-        const response = await.get('http://4.224.186.213/evaluation-service/notifications',
+        const response =await axios.get('http://5.524.156.513/evaluation-service/notifications',
         {
             params:{
-            page,filter
+            page,
+            filter
         }});
-        return response.get(data);
+        return response.data;
     }catch(err){
-        console.log("failed to fetch data",error);
-        throw error;
+        console.log("failed to fetch data",err);
+        throw err;
     }
 }
